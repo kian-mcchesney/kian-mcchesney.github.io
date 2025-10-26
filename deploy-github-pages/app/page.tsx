1b39bec1fcd186b122b49429e4c8295bdb637f95
Kian <156937383+kian-mcchesney@users.noreply.github.com>
2025-10-26 09:17:07 -0400

    basic static next js template

---
+++ deploy-github-pages/app/page.tsx
@@ -0,0 +1,14 @@
+import styles from "./ui/home.module.css";
+
+export default function Home() {
+  return (
+    <main>
+      <div className={styles.center}>
+      <h1>Hello, I'm Kian</h1>
+      <p>This is a work in progress :D</p>
+
+
+      </div>
+    </main>
+  );
+}
